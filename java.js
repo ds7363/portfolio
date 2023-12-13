7df0dd1f24a0af5a8a244667dc9e4219876ecfe6
Darko Skulikj <ds7363@nyu.edu>
2023-12-14 02:53:32 +0400

    final update 1

--- java.js
+++ java.js
@@ -1,28 +1,61 @@
-// Define selector for selecting
-// anchor links with the hash
-let anchorSelector = 'a[href^="#"]';
-
-// Collect all such anchor links
-let anchorList =
-    document.querySelectorAll(anchorSelector);
-    
-// Iterate through each of the links
-anchorList.forEach(link => {
-    link.onclick = function (e) {
-
-        // Prevent scrolling if the
-        // hash value is blank
+document.addEventListener('DOMContentLoaded', function () {
+    // Smooth scrolling for anchor links
+    let anchorSelector = 'a[href^="#"]';
+    let anchorList = document.querySelectorAll(anchorSelector);
+  
+    anchorList.forEach(link => {
+      link.addEventListener('click', function (e) {
         e.preventDefault();
-    
-        // Get the destination to scroll to
-        // using the hash property
-        let destination =
-            document.querySelector(this.hash);
-    
-        // Scroll to the destination using
-        // scrollIntoView method
+        let destination = document.querySelector(this.hash);
         destination.scrollIntoView({
-            behavior: 'smooth'
+          behavior: 'smooth'
         });
-    }
+      });
+    });
+  
+    // Sound on hover
+    const hoverSound = new Audio('gamesound.mp3');
+  
+    document.querySelectorAll('.hover-sound').forEach(function (image) {
+      image.addEventListener('mouseover', function () {
+        hoverSound.currentTime = 0;
+        hoverSound.play();
+      });
+  
+      image.addEventListener('mouseout', function () {
+        hoverSound.pause();
+      });
+    });
+
+    // Sound on hover
+    const hoverSound1 = new Audio('gym.mp3' );
+  
+    document.querySelectorAll('.hover-sound1').forEach(function (image) {
+      image.addEventListener('mouseover', function () {
+        hoverSound1.currentTime = 0;
+        hoverSound1.play();
+      });
+  
+      image.addEventListener('mouseout', function () {
+        hoverSound1.pause();
+    });
 });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const fireDiv = document.getElementById('toggleFire');
+    const bunseenDiv = document.getElementById('Bunseen');
+    const flagDiv = document.getElementById('Flag');
+  
+    fireDiv.addEventListener('click', function () {
+      bunseenDiv.classList.toggle('visible');
+      flagDiv.classList.toggle('visible');
+    });
+  });
+  
+  
+
+  
+  
+  
+  
